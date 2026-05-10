450856800b1b97b371e8320ecd1b5e378b583db9
carlmoreau76-design <279212039+carlmoreau76-design@users.noreply.github.com>
2026-05-10 15:37:18 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -1,10 +1,28 @@
 const { commands } = global.GoatBot;
 const config = global.GoatBot.config;
 
+// petit convertisseur italic
+function toItalic(text) {
+  const map = {
+    a: "𝘢", b: "𝘣", c: "𝘤", d: "𝘥", e: "𝘦",
+    f: "𝘧", g: "𝘨", h: "𝘩", i: "𝘪", j: "𝘫",
+    k: "𝘬", l: "𝘭", m: "𝘮", n: "𝘯", o: "𝘰",
+    p: "𝘱", q: "𝘲", r: "𝘳", s: "𝘴", t: "𝘵",
+    u: "𝘶", v: "𝘷", w: "𝘸", x: "𝘹", y: "𝘺", z: "𝘻",
+    A: "𝘈", B: "𝘉", C: "𝘊", D: "𝘋", E: "𝘌",
+    F: "𝘍", G: "𝘎", H: "𝘏", I: "𝘐", J: "𝘑",
+    K: "𝘒", L: "𝘓", M: "𝘔", N: "𝘕", O: "𝘖",
+    P: "𝘗", Q: "𝘘", R: "𝘙", S: "𝘚", T: "𝘛",
+    U: "𝘜", V: "𝘝", W: "𝘞", X: "𝘟", Y: "𝘠", Z: "𝘡"
+  };
+
+  return text.split("").map(c => map[c] || c).join("");
+}
+
 module.exports = {
   config: {
     name: "help",
-    version: "7.3",
+    version: "7.4",
     author: "Shade",
     countDown: 2,
     role: 0,
@@ -39,7 +57,7 @@ module.exports = {
 
       menu += categories[cat]
         .sort()
-        .map(c => `╎ ✧ ${c}`)
+        .map(c => `╎ ✧ ${toItalic(c)}`) // 👈 ICI MODIF
         .join("\n");
 
       menu += `
@@ -61,7 +79,6 @@ module.exports = {
         attachment: await global.utils.getStreamFromURL(imageURL)
       });
     } catch (e) {
-      // fallback si image bug
       return message.reply(menu);
     }
   }
