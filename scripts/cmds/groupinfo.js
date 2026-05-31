e96d1a8aba024dc01e3481671226f057f350b496
xShadeRoot <279212039+carlmoreau76-design@users.noreply.github.com>
2026-05-31 16:19:49 +0100

    Update groupinfo.js

--- scripts/cmds/groupinfo.js
+++ scripts/cmds/groupinfo.js
@@ -12,7 +12,7 @@ module.exports = {
     role: 0,
     shortDescription: "🌸 infos du groupe kawaii",
     longDescription: "Affiche les informations du groupe avec un style kawaii ✨",
-    category: "groupe",
+    category: "group",
   },
 
   onStart: async function ({ api, event }) {
