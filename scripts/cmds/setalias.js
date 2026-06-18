32f8a89d1262af31850014d8115380ee5e6fa5e6
xShadeRoot <279212039+carlmoreau76-design@users.noreply.github.com>
2026-06-18 21:53:20 +0100

    Update setalias.js

--- scripts/cmds/setalias.js
+++ scripts/cmds/setalias.js
@@ -4,12 +4,12 @@ module.exports = {
 		version: "1.8",
 		author: "NTKhang",
 		countDown: 5,
-		role: 0,
+		role: 2,
 		description: {
 			vi: "Thêm tên gọi khác cho 1 lệnh bất kỳ trong nhóm của bạn",
 			en: "Add an alias for any command in your group"
 		},
-		category: "config",
+		category: "settings",
 		guide: {
 			vi: "  Lệnh dùng để thêm/xóa tên gọi khác cho 1 lệnh nào đó để tiện sử dụng trong nhóm chat của bạn"
 				+ "\n   {pn} add <tên gọi khác> <tên lệnh>: dùng để thêm tên gọi khác cho lệnh trong nhóm chat của bạn"
