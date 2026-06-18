da2edeebd9a8462ba6b57c82907fba2cac2bd19f
xShadeRoot <279212039+carlmoreau76-design@users.noreply.github.com>
2026-06-18 21:10:23 +0100

    Update setwelcome.js

--- scripts/cmds/setwelcome.js
+++ scripts/cmds/setwelcome.js
@@ -7,12 +7,12 @@ module.exports = {
 		version: "1.7",
 		author: "NTKhang",
 		countDown: 5,
-		role: 1,
+		role: 2,
 		description: {
 			vi: "Chỉnh sửa nội dung tin nhắn chào mừng thành viên mới tham gia vào nhóm chat của bạn",
 			en: "Edit welcome message content when new member join your group chat"
 		},
-		category: "custom",
+		category: "admin",
 		guide: {
 			vi: {
 				body: "   {pn} text [<nội dung> | reset]: chỉnh sửa nội dung văn bản hoặc reset về mặc định, với những shortcut có sẵn:"
